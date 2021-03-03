720233199541a385aa324c02f59205928a09c86f
Matthew Cain <yooksi@zoho.com>
2021-03-03 04:23:12 +0100

    Implement logging system

---
+++ media/lua/client/ModLogger.lua
@@ -0,0 +1,44 @@
+---@class ModLogger
+ModLogger = {}
+
+---@type string
+local name
+
+---@type ZLogger
+local logger
+
+---@param loggerName string
+function ModLogger:initialize(loggerName)
+
+	name = loggerName
+	logger = ZLogger.new(name, true)
+	if ModLogger.logger == nil then
+		print('ERROR: Unable to create ZLogger instance (' .. name .. ')')
+	else
+		ModLogger:debug('Initialized logger for name ' .. name)
+	end
+end
+
+local function write(msg, level)
+	logger:write(name .. ': ' .. msg, level)
+end
+
+---@param msg string
+function ModLogger:error(msg)
+	write(msg, "ERROR")
+end
+
+---@param msg string
+function ModLogger:warn(msg)
+	write(msg, "WARN")
+end
+
+---@param msg string
+function ModLogger:info(msg)
+	write(msg, "INFO")
+end
+
+---@param msg string
+function ModLogger:debug(msg)
+	write(msg, "DEBUG")
+end
