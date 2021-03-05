ead7092da0f2f74921140227429989a2526d9aa0
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-03-04 21:02:34 -0500

    Refactor Vector2 homing test

--- src/test/java/io/cocolabs/ehe/Vector2Test.java
+++ src/test/java/io/cocolabs/ehe/Vector2Test.java
@@ -5,15 +5,32 @@ import zombie.GameTime;
 import zombie.iso.IsoUtils;
 import zombie.iso.Vector2;
 
+import java.util.Random;
+
 class Vector2Test {
+
+    private static final Random RAND = new Random();
 
     @Test
     void shouldHomeToTargetPosition() {
 
-        Vector2 helicopter = new Vector2(1, 1);
-        Vector2 player = new Vector2(100, 250);
+        Vector2 player = getRandomVector(75, 250);
+        Helicopter helicopter = new Helicopter();
 
-        while(helicopter.distanceTo(player) > 1)
+        for (int i = 0; helicopter.getDistanceTo(player) > 1; i++)
+        {
+            helicopter.moveToPosition(player);
+            System.out.println("i:" + i + ' ' + helicopter);
+        }
+    }
+
+    @Test
+    void shouldHomeToMovingTargetPosition() {
+
+        Vector2 helicopter = new Vector2(1, 1);
+        Vector2 player = getRandomVector(75, 250);
+
+        for (int i = 0; helicopter.distanceTo(player) > 1; i++)
         {
             Vector2 cloned = helicopter.clone();
             cloned.aimAt(player);
@@ -24,7 +41,11 @@ class Vector2Test {
             helicopter.x += cloned.x;
             helicopter.y += cloned.y;
 
-            System.out.println(helicopter);
+            System.out.println("i:" + i + ' ' + helicopter);
         }
+    }
+
+    private static Vector2 getRandomVector(int min, int max) {
+        return new Vector2(RAND.nextInt(max) + min, RAND.nextInt(max) + min);
     }
 }
