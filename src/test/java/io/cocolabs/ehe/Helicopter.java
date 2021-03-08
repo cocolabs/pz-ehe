760f6fb604f0fafb7168d1531579ea4e5221cd09
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-03-07 21:20:50 -0500

    Implement movement smoothing

--- src/test/java/io/cocolabs/ehe/Helicopter.java
+++ src/test/java/io/cocolabs/ehe/Helicopter.java
@@ -26,6 +26,8 @@ public class Helicopter {
         movement.normalize();
         movement.setLength(speed);
 
+        movement.x *= ((destination.x - position.x)/destination.x);
+        movement.y *= ((destination.y - position.y)/destination.y);
 
         float lastDistance = position.distanceTo(destination);
 
